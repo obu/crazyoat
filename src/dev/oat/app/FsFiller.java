786c60567a3e39c43bac2be0a56fb7125f97ea88
obu <obu@nimblestorage.com>
2012-11-29 17:17:02 -0800

    planning

--- src/dev/oat/app/FsFiller.java
+++ src/dev/oat/app/FsFiller.java
@@ -5,10 +5,21 @@ package dev.oat.app;
  * An application to generate controllable data to fill up a file system
  * @author obu
  */
-public class FsFiller {
+public class FsFiller extends OatApp {
 
 
     public static void main(String[] args) {
-        // TODO code application logic here
+        //Task: I want to fill a file system
+        // with 1 Billion files, 
+        // and the total size of data is 10TB.
+        
+        /*
+         * For the app to scale, multi-threaded and multi-node programming
+         * is needed.  Each node can do a subset of the job, and they can work
+         * together in an independent manner.
+         */
+        
+        FsFiller fsf = new FsFiller(args);
+        
     }
 }
